c93e9dd20dee8422afc6b5ededb8d4e3ef9e4571
Ciro Rocha <o.cirorocha@gmail.com>
2020-08-01 08:49:29 -0300

    tail wig

--- animation-properties/src/App.js
+++ animation-properties/src/App.js
@@ -6,17 +6,25 @@ function App() {
   const [currentAnimation, setCurrentAnimation] = useState('')
   return (
     <div className={ 'App ' + currentAnimation }>
-      <menu>
-        <button id="run" onClick={() => setCurrentAnimation(currentAnimation + ' running')} >Run</button>
-        <button id="sit" onClick={() => setCurrentAnimation(currentAnimation + ' sitting')} >Sit</button>
-        <button id="off" onClick={() => setCurrentAnimation('')} >Animation Off</button>
-      </menu>
-      <div class="announce">
-        <p>Ready</p>
-        <p>Set</p>
-        <p>Go!</p>
+      <div  className={ currentAnimation } style={{ marginBottom: '7rem' }} >
+        <menu>
+          <button id="run" onClick={() => setCurrentAnimation('running')} >Run</button>
+          <button id="sit" onClick={() => setCurrentAnimation('sitting')} >Sit</button>
+          <button id="off" onClick={() => setCurrentAnimation('')} >Animation Off</button>
+        </menu>
+        <div class="announce">
+          <p>Ready</p>
+          <p>Set</p>
+          <p>Go!</p>
+        </div>
+        <div class="tuna"></div>
       </div>
-      <div class="tuna"></div>
+
+      <div  className='stage' style={{ margin: '7rem 0' }} >
+        <div class="window">
+          <div class="tail"></div>
+        </div>
+      </div>
     </div>
   );
 }
